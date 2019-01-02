bcdeaa8944f3d3eb79f2c7f6e0af516016616dce
Pia Eva Greiner <Pia@Pias-MacBook-Pro-2.local>
2019-01-01 20:49:11 -0500

    added selenium to requirements

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name='umdriver',
-    version='0.0.1',
+    version='0.0.2',
     author='Jamie Davis',
     author_email='jamjam@umich.edu',
     description=(
         'An extension of the selenium webdriver bindings for python '
         'with U-M weblogin baked in.'),
+    install_requires=[
+        'selenium',
+    ],
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/jamie-r-davis/umdriver',
